a8ac6a7d5f06a68111ccf239c45ab2bbf7fc432e
alice bor <40145195+alicebor94@users.noreply.github.com>
2019-04-26 16:38:43 +0300

    ProfileFragment added
    ProfileListAdapter added
    ProfileFragment added
    ProfileFragment XML changed

--- app/src/main/java/com/example/TripitAndroid/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/TripitAndroid/Fragments/ProfileFragment.java
@@ -11,15 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.TripitAndroid.Classes.Adapters.PostsListAdapter;
+import com.example.TripitAndroid.Classes.Adapters.ProfileListAdapter;
 import com.example.TripitAndroid.Classes.Post;
-import com.example.TripitAndroid.Classes.UserInfo;
 import com.example.TripitAndroid.R;
 import com.example.TripitAndroid.models.FirebaseModel;
 import com.example.TripitAndroid.models.Model;
-import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.Vector;
@@ -33,7 +30,7 @@ public class ProfileFragment extends Fragment {
     private TextView mTextMessage;
     RecyclerView mRecyclerView;
     RecyclerView.LayoutManager mLyoutManager;
-    PostsListAdapter mAdapter;
+    ProfileListAdapter mAdapter;
     Vector<String> mData = new Vector<String>();
 
     public ProfileFragment() {
@@ -46,32 +43,17 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        mRecyclerView = (RecyclerView)view.findViewById(R.id.main_recyclerview);
+        mRecyclerView = (RecyclerView)view.findViewById(R.id.Profile_recyclerview);
 
 
 
-//        FirebaseUser user = Model.instance.currentUser();
-//        if (user != null) {
-//            String email = user.getEmail();
-//            String uid = user.getUid();
-//
-//            Model.instance.getUserInfo(uid, new FirebaseModel.OnGetUserInfoCompletedListener() {
-//                @Override
-//                public void onUserInfoGetComplete(UserInfo userInfo) {
-//                    if (userInfo != null) {
-//
-//                    }
-//                }
-//            });
-//
-//            Toast.makeText(getActivity(), email, Toast.LENGTH_LONG).show();
-//        }
+        String uid = Model.instance.currentUser().getUid();
 
-
-        Model.instance.getAllPostsFromUser("fiNmpX6PbxRxAAZnrVfEWETFSY53", new FirebaseModel.OnGetUserPostsCompleteListener() {
+        Model.instance.getAllPostsFromUser(uid, new FirebaseModel.OnGetUserPostsCompleteListener() {
             @Override
             public void OnGetUserPostsComplete(ArrayList<Post> data) {
-                mAdapter = new PostsListAdapter(data);
+                mAdapter = new ProfileListAdapter(data);
+
 
                 RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
                 mRecyclerView.setLayoutManager(layoutManager);
